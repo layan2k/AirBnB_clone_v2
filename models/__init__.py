a8e54f8f5324a918d1a374d56e4590c3fb098e45
layan2k <lesliedeeshumba@gmail.com>
2021-12-20 22:28:19 +0200

    complete

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,12 @@
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
+storage_t = HBNB_TYPE_STORAGE
 
-storage = FileStorage()
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String, ForeignKey
 
 class Amenity(BaseModel):
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
