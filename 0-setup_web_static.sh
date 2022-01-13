83c88ceeca56a934fb109b7bb9a6ab77dc525932
layan2k <lesliedeeshumba@gmail.com>
2022-01-13 10:57:12 +0200

    script correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,7 @@
 #script that configures NGINX Folders and files
 sudo apt-get -y update
 sudo apt-get install nginx
-sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "Dummy Text" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
