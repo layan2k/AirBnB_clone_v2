fcd9636e2d7059563b8e04d7166cf3493462e1fe
layan2k <lesliedeeshumba@gmail.com>
2022-01-13 12:06:17 +0200

    code mode

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,18 +3,20 @@
    using the function do_clean"""
 
 from fabric.api import *
-
+import os
+import shlex
 
 env.user = 'ubuntu'
 env.hosts = ['3.238.253.91', '35.237.111.104']
 
 
 def do_clean(number=0):
-    if number == 0:
-        number = 1
-    with cd.local('./versions'):
-        local("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
-            xargs -d '\n' rm".format(1 + number))
-    with cd('/data/web_static/releases/'):
-        run("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
-            xargs -d '\n' rm".format(1 + number))
+    if os.path.exists('versions'):
+        if int(number) == 0:
+            variable = 2
+        else:
+            variable = int(number) + 1
+        command = 'tail -n +{}| xargs rm -rf'.format(variable)
+        local('cd versions ; ls -t|{}'.format(command))
+        dir = '/data/web_static/releases'
+        run('cd {}; ls -t| grep web_static|{}'.format(dir, command))
