4f44150f8d274655adbdba9f81af0595d8ec07fd
layan2k <lesliedeeshumba@gmail.com>
2022-01-13 11:09:45 +0200

    fix 3

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 #script that configures NGINX Folders and files
+
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "Dummy Text" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
