c8d478aea772b8e3e35998e4583f62e568846774
layan2k <lesliedeeshumba@gmail.com>
2022-01-13 11:01:16 +0200

    fix3

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,7 +14,7 @@ def do_pack():
     try:
         currentdate = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
-            local("mkdir versions")
+            local("mkdir -p versions")
             file_name = "versions/web_static_{}.tgz".format(currentdate)
             local("tar -cvzf {} web_static".format(file_name))
         return file_name
