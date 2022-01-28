27c620b9201dc8a32e69d2411c24d0fad73d713c
layan2k <lesliedeeshumba@gmail.com>
2022-01-29 00:40:58 +0200

    evenodd

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ Returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext(text):
+    """Returns ,C <text>"""
+    text = text
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def ctext(text="is_cool"):
+    """Returns Python <text> Default= is_cool"""
+    text = text
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>")
+def isint(n):
+    """return n if interger"""
+    return ("{} is a number".format(n))
+
+
+@app.route("/number/<int:n>")
+def isint(n):
+    """return n if interger, renders template"""
+    return render_template("5-number.html", number=n)
+
+
+@app.route("/number/<int:n>")
+def isevenodd(n):
+    """return n if interger, renders template"""
+    return render_template("6-number_odd_or_even.html", number=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
