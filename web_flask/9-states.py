b676626e98323b605aee276042da3565896a3660
layan2k <lesliedeeshumba@gmail.com>
2022-01-29 23:25:08 +0200

    states+id

---
+++ web_flask/9-states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+
+from models import storage
+from models import *
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tearDown(self):
+    """Closes each SQLAlchemyTask"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def display_html(state_id=None):
+    """Displays html page"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
