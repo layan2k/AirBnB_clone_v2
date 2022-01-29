96534a0956051942cfe027f465774791ecae5691
layan2k <lesliedeeshumba@gmail.com>
2022-01-29 22:58:36 +0200

    city states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+
+from models import storage
+from models import *
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tearDown(self):
+    """Closes each SQLAlchemyTask"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def display_html():
+    """Displays html page"""
+    state = sorted(list(storage.all("State").values()),
+                       key=lambda x: x.name)
+    return render_template("8-cities_by_states.html", state=state)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
