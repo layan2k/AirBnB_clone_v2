bc2d6a29f2cf8e6d73a93a8f55665345bed8efae
layan2k <lesliedeeshumba@gmail.com>
2022-01-29 17:09:21 +0200

    strict update + task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+
+from models import storage
+from models import *
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ Returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext(text):
+    """Returns HBNB"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythontext(text="is cool"):
+    """Returns Python <text> Default= is cool"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def isint(n):
+    """return n if interger"""
+    return ("{:d} is a number".format(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def isnumber(n):
+    """return n if interger, renders template"""
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def isevenodd(n):
+    """return n if interger, renders template"""
+    return render_template("6-number_odd_or_even.html", number=n)
+
+
+@app.teardown_appcontext
+def tearDown():
+    """Closes each SQLAlchemyTask"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_html():
+    """Displays html page"""
+    stateobjs = [s for s in storage.all("State").values()]
+    return render_template("7-states_list.html", stateobjs=stateobjs)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
